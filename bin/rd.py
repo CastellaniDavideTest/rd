d8bb8a69472f9beff3cfbedfdadba17858cc0a22
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-15 12:33:10 +0100

    Created bin/rd.py

---
+++ bin/rd.py
@@ -0,0 +1,19 @@
+"""rd
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-15"
+
+class rd:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(rd.rd())
+	
+	def rd():
+		"""rd first funtion
+		"""
+		return "rd" #for the test (see test_rd.py)
+		
+if __name__ == "__main__":
+	rd()
